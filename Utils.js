f3f5e3643e7162669b99f07920a2e89f295b6fc1
ace-1331 <ace7cups@gmail.com>
2020-03-21 23:35:27 -0400

    quick fix

--- Utils.js
+++ Utils.js
@@ -2,105 +2,107 @@
 
 //Starts the script
 function CursedStarter() {
-    window.cursedConfig = {
-        hasPublicAccess: true,
-        hasCursedBelt: false,
-        hasCursedKneel: false,
-        hasCursedLatex: false,
-        hasCursedSpeech: false,
-        hasCursedOrgasm: false,
-        hasCursedNakedness: false,
-        isMute: false,
-        disaledOnMistress: false,
-        enabledOnMistress: false,
-        hasCursedBlindfold: false,
-        hasCursedHood: false,
-        hasCursedEarplugs: false,
-        hasCursedDildogag: false,
-        hasCursedPanties: false,
-        hasCursedGag: false,
-        hasCursedMittens: false,
-        hasCursedBunny: false,
-        hasEntryMsg: false,
-        hasFullMuteChat: false,
-        hasCursedScrews: false,
+    try {
+        window.cursedConfig = {
+            hasPublicAccess: true,
+            hasCursedBelt: false,
+            hasCursedKneel: false,
+            hasCursedLatex: false,
+            hasCursedSpeech: false,
+            hasCursedOrgasm: false,
+            hasCursedNakedness: false,
+            isMute: false,
+            disaledOnMistress: false,
+            enabledOnMistress: false,
+            hasCursedBlindfold: false,
+            hasCursedHood: false,
+            hasCursedEarplugs: false,
+            hasCursedDildogag: false,
+            hasCursedPanties: false,
+            hasCursedGag: false,
+            hasCursedMittens: false,
+            hasCursedBunny: false,
+            hasEntryMsg: false,
+            hasFullMuteChat: false,
+            hasCursedScrews: false,
         
-        owners: Player.Ownership ? [Player.Ownership.MemberNumber.toString()] : [],
-        mistresses: Player.Ownership ? [Player.Ownership.MemberNumber.toString()] : [],
-        enforced: Player.Ownership ? [Player.Ownership.MemberNumber.toString()] : [],
-        blacklist: [...Player.BlackList],
-        bannedWords: [],
-        cursedItems: [],
-        entryMsg: "",
-        say: "",
-        mistressIsHere: false,
-        ownerIsHere: false,
+            owners: Player.Ownership ? [Player.Ownership.MemberNumber.toString()] : [],
+            mistresses: Player.Ownership ? [Player.Ownership.MemberNumber.toString()] : [],
+            enforced: Player.Ownership ? [Player.Ownership.MemberNumber.toString()] : [],
+            blacklist: [...Player.BlackList],
+            bannedWords: [],
+            cursedItems: [],
+            entryMsg: "",
+            say: "",
+            mistressIsHere: false,
+            ownerIsHere: false,
     
-        slaveIdentifier: Player.Name,
-        commandChar: "#",
+            slaveIdentifier: Player.Name,
+            commandChar: "#",
     
-        strikes: 0,
-        lastPunishmentAmount: 0,
-        lastWardrobeLock: 0,
-        strikeStartTime: Date.now(),
+            strikes: 0,
+            lastPunishmentAmount: 0,
+            lastWardrobeLock: 0,
+            strikeStartTime: Date.now(),
     
-        isRunning: false,
-        isSilent: false,
-        hasIntenseVersion: false,
-        chatlog: [],
-        log: [],
-    };
+            isRunning: false,
+            isSilent: false,
+            hasIntenseVersion: false,
+            chatlog: [],
+            log: [],
+        };
     
-    window.currentVersion = 11;
-    window.oldStorage = null;
-    window.oldVersion = null;
+        window.currentVersion = 11;
+        window.oldStorage = null;
+        window.oldVersion = null;
     
-    try { 
-        oldStorage = JSON.parse(localStorage.getItem(`bc-cursedConfig-${Player.MemberNumber}`));
-        oldVersion = JSON.parse(localStorage.getItem(`bc-cursedConfig-version-${Player.MemberNumber}`));
-    } catch { }
-    
-    //Pull config from log or create
-    if (!oldStorage) {
-        SendChat("The curse awakens on " + Player.Name + ".");
-        popChatSilent("Welcome to the curse! The curse allows for many mysterious things to happen... have fun discovering them. The help command should be able to get you started (" + cursedConfig.commandChar + cursedConfig.slaveIdentifier + " help). Please report any issues or bug you encounter to ace (12401) - Ace__#5558.");
-        try { 
-            localStorage.setItem(`bc-cursedConfig-version-${Player.MemberNumber}`, currentVersion);
+        try {
+            oldStorage = JSON.parse(localStorage.getItem(`bc-cursedConfig-${Player.MemberNumber}`));
+            oldVersion = JSON.parse(localStorage.getItem(`bc-cursedConfig-version-${Player.MemberNumber}`));
         } catch { }
-    } else { 
-        //Load previous data, takes care of upgrades or downgrades
-        if (oldVersion != currentVersion) { 
-            cursedConfig = { ...cursedConfig, ...oldStorage };
-            SendChat("The curse following " + Player.Name + " has changed.");
-            popChatSilent("You have loaded an updated version of the curse, please report any new bugs. This update may have introduced new features or bug fixes, don't forget to use the help command to see the available commands. (" + cursedConfig.commandChar + cursedConfig.slaveIdentifier + " help)");
-            localStorage.setItem(`bc-cursedConfig-version-${Player.MemberNumber}`, currentVersion);
-        } else if (oldVersion == currentVersion) { 
-            cursedConfig = oldStorage;
-            SendChat("The curse follows " + Player.Name + ".");
-            popChatSilent("Have fun~ Please report any issues or bug you encounter to ace (12401) - Ace__#5558.");
-        } 
-    }
     
-    //Cleans the existing chatlog
-    document.querySelectorAll('.ChatMessage:not([verified=true]').forEach(msg => { 
-        var verifiedAtt = document.createAttribute("verified");
-        verifiedAtt.value = "true";
-        msg.setAttributeNode(verifiedAtt);
-    });
+        //Pull config from log or create
+        if (!oldStorage) {
+            SendChat("The curse awakens on " + Player.Name + ".");
+            popChatSilent("Welcome to the curse! The curse allows for many mysterious things to happen... have fun discovering them. The help command should be able to get you started (" + cursedConfig.commandChar + cursedConfig.slaveIdentifier + " help). Please report any issues or bug you encounter to ace (12401) - Ace__#5558.");
+            try {
+                localStorage.setItem(`bc-cursedConfig-version-${Player.MemberNumber}`, currentVersion);
+            } catch { }
+        } else {
+            //Load previous data, takes care of upgrades or downgrades
+            if (oldVersion != currentVersion) {
+                cursedConfig = { ...cursedConfig, ...oldStorage };
+                SendChat("The curse following " + Player.Name + " has changed.");
+                popChatSilent("You have loaded an updated version of the curse, please report any new bugs. This update may have introduced new features or bug fixes, don't forget to use the help command to see the available commands. (" + cursedConfig.commandChar + cursedConfig.slaveIdentifier + " help)");
+                localStorage.setItem(`bc-cursedConfig-version-${Player.MemberNumber}`, currentVersion);
+            } else if (oldVersion == currentVersion) {
+                cursedConfig = oldStorage;
+                SendChat("The curse follows " + Player.Name + ".");
+                popChatSilent("Have fun~ Please report any issues or bug you encounter to ace (12401) - Ace__#5558.");
+            }
+        }
     
-    //Runs the script
-    cursedConfig.isRunning = true;
-    CursedCheckUp(); //Initial check
-    ChatlogProcess(); //Chatlog handling
-    InitHelpMsg();
+        //Cleans the existing chatlog
+        document.querySelectorAll('.ChatMessage:not([verified=true]').forEach(msg => {
+            var verifiedAtt = document.createAttribute("verified");
+            verifiedAtt.value = "true";
+            msg.setAttributeNode(verifiedAtt);
+        });
     
-    // Sends a message to the server.. this is modified to allow tricks into it
-    function ServerSend(Message, Data) {
-        var isActivated = !(cursedConfig.mistressIsHere && cursedConfig.disaledOnMistress)
-            && ((cursedConfig.enabledOnMistress && cursedConfig.ownerIsHere) ||  !cursedConfig.enabledOnMistress )
-        if (Message == "ChatRoomChat" && Data.Type == "Chat" && cursedConfig.hasFullMuteChat && isActivated) return;
-        ServerSocket.emit(Message, Data);
-    }
+        //Runs the script
+        cursedConfig.isRunning = true;
+        CursedCheckUp(); //Initial check
+        ChatlogProcess(); //Chatlog handling
+        InitHelpMsg();
+    
+        // Sends a message to the server.. this is modified to allow tricks into it
+        ServerSend = function (Message, Data) {
+            var isActivated = !(cursedConfig.mistressIsHere && cursedConfig.disaledOnMistress)
+                && ((cursedConfig.enabledOnMistress && cursedConfig.ownerIsHere) || !cursedConfig.enabledOnMistress)
+            if (Message == "ChatRoomChat" && Data.Type == "Chat" && cursedConfig.hasFullMuteChat && isActivated) return;
+            ServerSocket.emit(Message, Data);
+        }
+    } catch { }
 }
 
 //Stops the script
