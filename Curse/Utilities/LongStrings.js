d2936bb29ff5f5f433f1c7cf675d726601574bde
Just-another-username <66268731+Just-another-username@users.noreply.github.com>
2020-06-01 21:50:39 +0100

    Help (#1)
    Turned helpfile into a function so anyone can call it

--- Curse/Utilities/LongStrings.js
+++ Curse/Utilities/LongStrings.js
@@ -1,19 +1,23 @@
 //************************************ LONG STRINGS ************************************//
-/** Initializes all the long strings needed to split them from the rest of the code */
-function InitHelpMsg() {
-    window.helpTxt = `<pre>Your calling ID: ${cursedConfig.commandChar + cursedConfig.slaveIdentifier}
+/** Long strings for Help, now turned into a function */
+
+
+function helpMsg( helpType ) 
+switch (helpType) {
+case "wearer":
+return `Your calling ID: ${cursedConfig.commandChar + cursedConfig.slaveIdentifier}
     ${ChatRoomCharacter.map(el => {return { Name: el.Name, isCursed: el.isCursed }}).filter(n => n.Name == cursedConfig.slaveIdentifier && n.isCursed).length > 1
     ? "WARNING: Potential clash with another character!" : ""}
 
-    //WEARER FUNCTIONS//
+    >>WEARER FUNCTION>>
 
-    >>> Information <<<
+    >>> Information >>>
     -help
     -showstrikes
     -showblacklist
     -listsentences
 
-    >>> Config Commands <<<
+    >>> Config Commands >>>
     *NEW*-hidedisplay
     *NEW*-restraintvanish
     -isclassic
@@ -27,31 +31,71 @@ function InitHelpMsg() {
     -forwardall
     -savecolors
     -wardrobev2
-    INTENSE V:-capture
 
-    >>>Speech Commands<<<
-    INTENSE:-talk [target id] [sentence id]
-    >>> Other Commands <<<
-    INTENSE V:-nickname [Number] [Name]
-    INTENSE V:-deletenickname [Number] [Name]
+    >>> Other Commands >>>
     -draw [nb of cards] [players]
     -shuffle
 
-    //PUBLIC FUNCTIONS//
-    -help
-    -respect
-    -punish
-    -edge
-    -asylumtimeleft
-    -readnote
-    -sendnote
-    INTENSE V:-nickname [Name]
-    INTENSE V:-blocknickname
-    INTENSE V:-allownickname
-    INTENSE V:-capture
+    >>>INTENSE V>>> -allowcapture
+    -talk [target id] [sentence id]
+    -nickname [Number] [Name]
+    -deletenickname [Number] [Name]`;
+        
+  case "clubowner":
+    return `
+    >>CLUB OWNER FUNCTIONS>>
+    >>>Configuration Commands>>>
+    INTENSE VERSION: -lockowner
+    *NEW*INTENSE VERSION: -locknewlover`;
+        
+  case "owner":
+    return `
+    >>OWNER FUNCTIONS>>
+    >>>Basic Commands>>>
+    -asylum [nb of hours]
+    -owner [a member number]
 
-    //MISTRESS FUNCTIONS//
-    >>> Basic Commands<<<
+    >>>Configuration Commands>>>
+    -guestnotes
+    -readnotes
+    -disablepunishments
+    -onlyonpresence
+    -restrainplay
+    -fullpublic
+    -afk
+    -note
+    *NEW*-reminderinterval [seconds]
+    *NEW*-clearallreminders
+
+    >>>Speech Commands>>>
+    -enforceentrymessage
+    -entrymessage [sentence]
+
+    >>>Curses>>>
+    -belt
+    *NEW*-reminders
+    *NEW*-togglereminder [reminder]
+
+    >>>INTENSE V>>> -onickname [Number] [Name]
+    -leash
+    -lockowner
+    -maid
+    *NEW* -maid
+    -restrainedspeech
+    -self [I, this slave, etc.]
+    -target [id] [target]
+    -sentence [id] [sentence]
+    -listsentences
+     -forcedsay [sentence]
+     -say [sentence]
+     -fullmute
+     -enablesound
+     -sound ["oink", "meow", ...]`;      
+        
+ case "mistress":
+    return `
+    >>MISTRESS FUNCTIONS>>
+    >>> Basic Commands>>>
     -kneel
     -cursereport
     -showstrikes
@@ -61,10 +105,8 @@ function InitHelpMsg() {
     -public
     -deactivateonpresence
     -savecolors
-    INTENSE V:-mnickname [Number] [Name]
-    INTENSE V:-deletenickname [Number]
 
-    >>>Speech Commands<<<
+    >>>Speech Commands>>>
     -mute
     -cursedspeech
     -banword [the word itself]
@@ -73,7 +115,7 @@ function InitHelpMsg() {
     -clearwords
     -contractions
     
-    >>>Curses<<<
+    >>>Curses>>>
     -naked
     -vibes
     -latex
@@ -91,67 +133,43 @@ function InitHelpMsg() {
     -rope
     -maiduniform
     -doublegag
-
-    //OWNER AND WEARER FUNCTIONS//
-    >>> Information <<<
+    >>>INTENSE V>>>
+    -mnickname [Number] [Name]
+    -deletenickname [Number]`;  
+        
+  case "private":
+    return `
+    >>OWNER AND WEARER FUNCTIONS>>
+    >>> Information >>>
     -speechreport
-    -showmistresses
-    -showowners
-    -showenforced
-    -shownicknames
-    -configreport
-
-    //OWNER FUNCTIONS//
-    >>>Basic Commands<<<
-    -asylum [nb of hours]
-    -owner [a member number]
-    INTENSE V:-onickname [Number] [Name]
-
-    >>>Configuration Commands<<<
-    -guestnotes
-    -readnotes
-    -disablepunishments
-    -onlyonpresence
-    -restrainplay
-    -fullpublic
-    -afk
-    -note
-    *NEW*-reminderinterval [seconds]
-    *NEW*-clearallreminders
-    INTENSE VERSION: -leash
-    INTENSE VERSION: -maid
-    *NEW*INTENSE VERSION: -sensdep
-
-    >>>Speech Commands<<<
-    -enforceentrymessage
-    -entrymessage [sentence]
-    INTENSE V:-restrainedspeech
-    INTENSE V:-self [I, this slave, etc.]
-    INTENSE V:-target [id] [target]
-    INTENSE V:-sentence [id] [sentence]
-    INTENSE V:-listsentences
-    INTENSE V: -forcedsay [sentence]
-    INTENSE V: -say [sentence]
-    INTENSE V: -fullmute
-    INTENSE V: -enablesound
-    INTENSE V: -sound ["oink", "meow", ...]
-
-    >>>Curses<<<
-    -belt
-    *NEW*-reminders
-    *NEW*-togglereminder [reminder]
-    
-    //CLUB OWNER FUNCTIONS//
-    >>>Configuration Commands<<<
-    INTENSE VERSION: -lockowner
-    *NEW*INTENSE VERSION: -locknewlover
-    
+    - showmistresses
+    - showowners
+    - showenforced
+    - shownicknames
+    - configreport`;
+        
+  case "public":
+    return `
+    >>PUBLIC FUNCTIONS>>
+    -help
+    -respect
+    -punish
+    -edge
+    -asylumtimeleft
+    -readnote
+    *NEW*-sendnote
+    >>>INTENSE V>>> -nickname [Name]
+    -blocknickname
+    -allownickname
+    -capture
     ---------
     More on various features:
     https://github.com/ace-1331/
     ace12401-cursedscript/wiki
     ---------
     Made by ace (12401) - Ace__#5558
-    Official release: V${currentVersion}
-    </pre>`;
+    Official release: V${currentVersion}`;
+        
+  default:
+  break;
 }
