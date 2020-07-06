20346826a748561838007ea1a44699a98d306d06
Ace <>
2020-07-06 19:46:23 -0400

    Update Utils.js

--- Utils.js
+++ Utils.js
@@ -1,7 +1,7 @@
 //************************************Callbacks************************************
 
 //Boot up sequence
-window.currentManifestVersion = "1.2.3.1";
+window.currentManifestVersion = "1.2.3.4";
 window.currentVersion = 36;
 let AlwaysOn;
 let isLoaded;
