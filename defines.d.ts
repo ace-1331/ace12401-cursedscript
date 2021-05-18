4f44d72ac41dbe8165ed019bd8ddd27cb255a15e
jomshir98 <jomshir98@protonmail.com>
2021-05-18 02:01:40 +0200

    JSConfig

---
+++ defines.d.ts
@@ -0,0 +1,142 @@
+// Curse globals
+var currentManifestVersion: string;
+var currentVersion: number;
+var cursedConfigInit: any;
+var cursedConfig: any;
+var oldStorage: any;
+var oldVersion: any;
+var vibratorGroups: any;
+var brokenVibratingItems: any;
+var curseTips: any[];
+var cursedPunishments: any[];
+var helpTxt: string;
+var Commands: any[];
+
+var CommandSet: any;
+var CommandCombine: any;
+
+var _: {
+	(text: any, ...args: any[]): any;
+	translate: any,
+	setFormatter(newFormatter: any): void;
+	format(...rest: any[]): string;
+	setDynamicTranslator(newDynoTrans: any): void;
+	setTranslation(newTranslation: any): void;
+}
+
+// BC Globals
+var CurrentScreen: string;
+var ChatRoomSpace: string;
+var Asset,
+	ActivityOrgasmGameResistCount,
+	ActivityOrgasmPrepare,
+	ActivityOrgasmStart,
+	AfkTimerSetIsAfk,
+	AnimationGetDynamicDataName,
+	AssetGroup,
+	AsylumBedroomLoad,
+	BackgroundsGenerateList,
+	BackgroundsTagAsylum,
+	BackgroundsTagList,
+	CellKeyDepositStaff,
+	CellLoad,
+	CellLock,
+	CellOpenTimer,
+	Character,
+	CharacterAppearanceLoadCharacter,
+	CharacterAppearanceReturnModule,
+	CharacterAppearanceReturnRoom,
+	CharacterDeleteAllOnline,
+	CharacterItemsHavePose,
+	CharacterLoadNPC,
+	CharacterRefresh,
+	CharacterReset,
+	CharacterSetActivePose,
+	CharacterSetCurrent,
+	CharacterSetFacialExpression,
+	ChatCreateBackgroundList,
+	ChatRoomAdminChatAction,
+	ChatRoomCharacter,
+	ChatRoomCharacterUpdate,
+	ChatRoomClearAllElements,
+	ChatRoomCurrentTime,
+	ChatRoomData,
+	ChatRoomDrawCharacterOverlay,
+	ChatRoomGame,
+	ChatRoomLastMessage,
+	ChatRoomLastMessageIndex,
+	ChatRoomListManipulation,
+	ChatRoomLovershipOptionIs,
+	ChatRoomOwnershipOptionIs,
+	ChatRoomPlayerCanJoin,
+	ChatRoomPlayerIsAdmin,
+	ChatRoomSendChat,
+	ChatRoomTarget,
+	ChatRoomTargetMemberNumber,
+	ChatSearchBackground,
+	ChatSearchLeaveRoom,
+	CommonIsMobile,
+	CommonSetScreen,
+	CurrentTime,
+	DialogFind,
+	DialogLeave,
+	DrawArousalMeter,
+	DrawButton,
+	DrawCharacter,
+	DrawCheckbox,
+	DrawText,
+	ElementCreateInput,
+	ElementCreateTextArea,
+	ElementFocus,
+	ElementIsScrolledToEnd,
+	ElementPosition,
+	ElementRemove,
+	ElementScrollToEnd,
+	ElementValue,
+	FriendListBeepLog,
+	GamblingRun,
+	InformationSheetLoadCharacter,
+	InventoryAdd,
+	InventoryAllow,
+	InventoryConfiscateKey,
+	InventoryConfiscateRemote,
+	InventoryDelete,
+	InventoryGet,
+	InventoryGroupIsBlocked,
+	InventoryOwnerOnlyItem,
+	InventoryRemove,
+	InventoryWear,
+	Log,
+	LogAdd,
+	LogDelete,
+	LogQuery,
+	LogValue,
+	MaidQuartersMaid,
+	MaidQuartersOnlineDrinkFromOwner,
+	MainCanvas,
+	MainHallClick,
+	MainHallMaid,
+	MainHallMaidReleasePlayer,
+	MainHallMaidShamePlayer,
+	MainHallRun,
+	ManagementAllowReleaseChastity,
+	ManagementCanBeReleasedOnline,
+	ManagementCanBreakTrialOnline,
+	ManagementCannotBeReleasedOnline,
+	NurseryRun,
+	PhotographicPlayerRelease,
+	Player,
+	ReputationCharacterGet,
+	ServerAccountBeep,
+	ServerBeep,
+	ServerPlayerAppearanceSync,
+	ServerPlayerLogSync,
+	ServerSend,
+	ServerSocket,
+	SpeechGarble,
+	TextGet,
+	VibratorModeScriptDraw,
+	WardrobeFastLoad,
+	WardrobeFastSave;
+
+var LZString;
